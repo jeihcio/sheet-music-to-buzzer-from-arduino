e288a1ad1f5aaee6e9a644b69d4264c0090d3d1d
Jeihcio Francis <jeihcio.francis@hotmail.com>
2023-01-13 14:01:05 -0300

    Add "write score"

--- ArduinoCodeGenerator/Main.Designer.cs
+++ ArduinoCodeGenerator/Main.Designer.cs
@@ -30,30 +30,123 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbFigure = new System.Windows.Forms.ComboBox();
+            this.numScale = new System.Windows.Forms.NumericUpDown();
+            this.cmbNote = new System.Windows.Forms.ComboBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblBPM = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.numberBPM = new System.Windows.Forms.NumericUpDown();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.lblBPM = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAddNote = new System.Windows.Forms.Button();
+            this.btnAddPause = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numScale)).BeginInit();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numberBPM)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.Controls.Add(this.btnAddPause);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.btnAddNote);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cmbFigure);
+            this.groupBox1.Controls.Add(this.numScale);
+            this.groupBox1.Controls.Add(this.cmbNote);
             this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.groupBox1.Location = new System.Drawing.Point(0, 300);
+            this.groupBox1.Location = new System.Drawing.Point(5, 288);
             this.groupBox1.Margin = new System.Windows.Forms.Padding(300);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(800, 150);
+            this.groupBox1.Size = new System.Drawing.Size(791, 71);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = " Add notes ";
+            this.groupBox1.Text = " Write Score ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Note";
+            // 
+            // cmbFigure
+            // 
+            this.cmbFigure.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbFigure.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbFigure.FormattingEnabled = true;
+            this.cmbFigure.Items.AddRange(new object[] {
+            "Semibreve",
+            "Minimum",
+            "Quarter Note",
+            "Eighth Note",
+            "Sixteenth Note",
+            "Fusa",
+            "Semifusa"});
+            this.cmbFigure.Location = new System.Drawing.Point(153, 42);
+            this.cmbFigure.Name = "cmbFigure";
+            this.cmbFigure.Size = new System.Drawing.Size(112, 21);
+            this.cmbFigure.TabIndex = 2;
+            // 
+            // numScale
+            // 
+            this.numScale.Location = new System.Drawing.Point(99, 42);
+            this.numScale.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numScale.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numScale.Name = "numScale";
+            this.numScale.Size = new System.Drawing.Size(48, 20);
+            this.numScale.TabIndex = 1;
+            this.numScale.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // cmbNote
+            // 
+            this.cmbNote.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbNote.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbNote.FormattingEnabled = true;
+            this.cmbNote.Items.AddRange(new object[] {
+            "C - Dó",
+            "C# - Dó#",
+            "D - Ré",
+            "D# - Ré#",
+            "E - Mi",
+            "F - Fá",
+            "F# - Fá#",
+            "G - Sol",
+            "G# - Sol #",
+            "A - Lá",
+            "A# - Lá #",
+            "B - Si"});
+            this.cmbNote.Location = new System.Drawing.Point(7, 41);
+            this.cmbNote.Name = "cmbNote";
+            this.cmbNote.Size = new System.Drawing.Size(86, 21);
+            this.cmbNote.TabIndex = 0;
             // 
             // panel1
             // 
@@ -68,6 +161,34 @@
             this.panel1.Size = new System.Drawing.Size(800, 250);
             this.panel1.TabIndex = 3;
             // 
+            // lblBPM
+            // 
+            this.lblBPM.AutoSize = true;
+            this.lblBPM.Location = new System.Drawing.Point(79, 51);
+            this.lblBPM.Name = "lblBPM";
+            this.lblBPM.Size = new System.Drawing.Size(19, 13);
+            this.lblBPM.TabIndex = 3;
+            this.lblBPM.Text = "60";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(52, 34);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(33, 35);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
+            this.panel2.Location = new System.Drawing.Point(51, 69);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1044, 112);
+            this.panel2.TabIndex = 1;
+            // 
             // pictureBox1
             // 
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
@@ -78,15 +199,6 @@
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            // 
-            // panel2
-            // 
-            this.panel2.AutoScroll = true;
-            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
-            this.panel2.Location = new System.Drawing.Point(51, 69);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1044, 112);
-            this.panel2.TabIndex = 1;
             // 
             // label1
             // 
@@ -115,30 +227,74 @@
             0});
             this.numberBPM.ValueChanged += new System.EventHandler(this.numberBPM_ValueChanged);
             // 
-            // pictureBox2
+            // label3
             // 
-            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
-            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox2.Location = new System.Drawing.Point(52, 34);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(33, 35);
-            this.pictureBox2.TabIndex = 2;
-            this.pictureBox2.TabStop = false;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(101, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Scale";
             // 
-            // lblBPM
+            // label4
             // 
-            this.lblBPM.AutoSize = true;
-            this.lblBPM.Location = new System.Drawing.Point(79, 51);
-            this.lblBPM.Name = "lblBPM";
-            this.lblBPM.Size = new System.Drawing.Size(19, 13);
-            this.lblBPM.TabIndex = 3;
-            this.lblBPM.Text = "60";
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(156, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Figure";
+            // 
+            // btnAddNote
+            // 
+            this.btnAddNote.Location = new System.Drawing.Point(270, 40);
+            this.btnAddNote.Name = "btnAddNote";
+            this.btnAddNote.Size = new System.Drawing.Size(27, 23);
+            this.btnAddNote.TabIndex = 6;
+            this.btnAddNote.Text = "+";
+            this.btnAddNote.UseVisualStyleBackColor = true;
+            // 
+            // btnAddPause
+            // 
+            this.btnAddPause.Location = new System.Drawing.Point(463, 39);
+            this.btnAddPause.Name = "btnAddPause";
+            this.btnAddPause.Size = new System.Drawing.Size(27, 23);
+            this.btnAddPause.TabIndex = 9;
+            this.btnAddPause.Text = "+";
+            this.btnAddPause.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(349, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Pause";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Semibreve",
+            "Minimum",
+            "Quarter Note",
+            "Eighth Note",
+            "Sixteenth Note",
+            "Fusa",
+            "Semifusa"});
+            this.comboBox1.Location = new System.Drawing.Point(346, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(112, 21);
+            this.comboBox1.TabIndex = 7;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 362);
             this.Controls.Add(this.numberBPM);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
@@ -149,11 +305,14 @@
             this.Name = "Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Generate code for Arduino";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numScale)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numberBPM)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -169,6 +328,16 @@
         private System.Windows.Forms.NumericUpDown numberBPM;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label lblBPM;
+        private System.Windows.Forms.ComboBox cmbNote;
+        private System.Windows.Forms.ComboBox cmbFigure;
+        private System.Windows.Forms.NumericUpDown numScale;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAddPause;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnAddNote;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
     }
 }
 
