9bff668319dc4aaddf1b7693290b5f47b70340c1
Jeihcio Francis <jeihcio.francis@hotmail.com>
2023-01-14 15:48:55 -0300

    Update NoteEnum.cs

--- ArduinoCodeGenerator/Entities/Enum/NoteEnum.cs
+++ ArduinoCodeGenerator/Entities/Enum/NoteEnum.cs
@@ -8,17 +8,12 @@ namespace ArduinoCodeGenerator.Entities.Enum
 {
     public enum NoteEnum
     {
-        NoteDo = 0,
-        //NoteDoSharp =1,
-        NoteRe = 2,
-        //NoteReSharp = 3,
-        NoteMi = 4,
-        NoteFa = 5,
-        //NoteFaSharp = 6,
-        NoteSol = 7,
-        //NoteSolSharp = 8,
-        NoteLa = 9,
-        //NoteLaSharp = 10,
-        NoteSi = 11
+        NoteDo,
+        NoteRe,
+        NoteMi,
+        NoteFa,
+        NoteSol,
+        NoteLa,
+        NoteSi
     }
 }
